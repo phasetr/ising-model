9befa1e4f66c7f6b0545580a15eeb4a28b533d8c
phasetr <phasetr@gmail.com>
2026-05-25 00:01:54 +0900

    Package pseudoMassFromParams local equation

--- IsingModel/Concrete/LatticeGraphCorrelation/Lemma_17_5_2.lean
+++ IsingModel/Concrete/LatticeGraphCorrelation/Lemma_17_5_2.lean
@@ -1,3 +1,4 @@
+import IsingModel.Concrete.LatticeGraphCorrelation.Lemma_17_5_2.PseudoMassFromParamsLocalEq
 import IsingModel.Concrete.LatticeGraphCorrelation.Lemma_17_5_2.FiniteHLSLipschitzPackage
 
 /-!
